cf07ad133aec6fca903ff7b35df528c2fd884733
Ubuntu <ubuntu@ip-172-31-25-170.ec2.internal>
2025-04-23 12:49:59 +0000

    commit2

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 
 // Start server on port 3000 or custom port via environment variable
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 
